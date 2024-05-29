88e307d30565d961a4eb44a63ff6884b06732364
drmdvld <razanilzan@gmail.com>
2024-05-29 19:26:09 +0700

    sprint 2

--- app/Http/Controllers/AntrianController.php
+++ app/Http/Controllers/AntrianController.php
@@ -8,11 +8,25 @@ use Illuminate\Http\Request;
 
 class AntrianController extends Controller
 {
-    public function index(){
-       $queues = Antrian::all()->map(function($queue) {
-        $queue->tanggal = Carbon::parse($queue->tanggal)->translatedFormat('d F Y');
+//     public function index(){
+//        $queues = Antrian::all()->map(function($queue) {
+//         $queue->tanggal = Carbon::parse($queue->tanggal)->translatedFormat('d F Y');
+//         return $queue;
+//     });
+//         return view('antrian.index', compact('queues'));
+// }
+
+
+public function index(){
+    $queues = JadwalPertemuan::all()->map(function($queue) {
+        $queue->nama_pasien = $queue->namadepan . ' ' . $queue->namabelakang;
+        $queue->keluhan = $queue->keluhanpasien;
+        $queue->tanggal = Carbon::parse($queue->tanggalpertemuan)->translatedFormat('d F Y');
+        $queue->est_jadwal = $queue->jampertemuan;
+        $queue->dokter = $queue->namadokter;
         return $queue;
     });
-        return view('antrian.index', compact('queues'));
+
+    return view('antrian.index', compact('queues'));
 }
 }
