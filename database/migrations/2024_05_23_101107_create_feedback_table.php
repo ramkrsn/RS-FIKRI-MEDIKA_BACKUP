8c8e73fdc3d04e559b6fd9e77619fd7c1eb5366f
Alifmm <maulanaalif216@gmail.com>
2024-06-08 00:25:40 +0700

    RPL450107-21 sprint3

--- database/migrations/2024_05_23_101107_create_feedback_table.php
+++ database/migrations/2024_05_23_101107_create_feedback_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFeedbackTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -32,3 +32,4 @@ class CreateFeedbackTable extends Migration
         Schema::dropIfExists('feedback');
     }
 }
+;
