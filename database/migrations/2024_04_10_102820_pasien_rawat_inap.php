33f0f010a6659ff3e4df50d0b8b01ccfaa527610
ramkrsn <guungramaa@gmail.com>
2024-06-12 14:26:44 +0700

    migration

--- database/migrations/2024_04_10_102820_pasien_rawat_inap.php
+++ database/migrations/2024_04_10_102820_pasien_rawat_inap.php
@@ -15,14 +15,14 @@ return new class extends Migration
             $table->id("idpasien")->primary();
             $table->string("namapasien");
             $table->bigInteger("nik")->unique();
-            $table->integer("nomerkamar");
+            $table->integer("nomerkamar")->nullable();
             $table->bigInteger("no_hp");
-            $table->integer("lantaikamar");
-            $table->string("penyakit");
-            $table->string("dokter");
-            $table->date("tanggal_masuk");
+            $table->integer("lantaikamar")->nullable();
+            $table->string("penyakit")->nullable();
+            $table->string("dokter")->nullable();
+            $table->date("tanggal_masuk")->nullable();
             $table->date("tanggal_keluar")->nullable();
-            $table->enum('status', ['rawat inap', 'rawat jalan', 'sudah pulang']);
+            $table->enum('status', ['rawat inap', 'rawat jalan', 'sudah pulang'])->nullable();
             $table->timestamps();
         });
     }
